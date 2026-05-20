5d4c4d1105c1cb4828ae4cfca177ccffcff71cfb
ma7me8 <ma7me8a7med@gmail.com>
2026-05-20 05:20:44 +0300

    finil edtion script

--- script.py
+++ script.py
@@ -8,7 +8,8 @@ DATA_START_ROW = 3
 KEY_COLUMN = "ROOM NUMBER"
 
 
-def read_floor_file(file_path: Path):          # this function to read all the doors in all floors
+# this function to read all the doors in all floors
+def read_floor_file(file_path: Path):
     workbook = openpyxl.load_workbook(file_path, data_only=True)
     sheet = workbook.active
 
@@ -17,26 +18,29 @@ def read_floor_file(file_path: Path):          # this function to read all the d
     for col in range(1, sheet.max_column + 1):
         header_name = sheet.cell(HEADER_ROW, col).value
         header[header_name] = col
-    
+
     floor_doors = {}
 
-    for row in range(DATA_START_ROW, sheet.max_row + 1):        # this loop to get all the doors in all floors
+    # this loop to get all the doors in all floors
+    for row in range(DATA_START_ROW, sheet.max_row + 1):
         room_number = sheet.cell(row, header[KEY_COLUMN]).value
 
         if not room_number:
             continue   # this condition to skip the rows if room number is None
 
-        door_data = {} 
+        door_data = {}
 
-        for header_name, col in header.items():    # this loop to get all header names and their values in all floors   
+        for header_name, col in header.items():    # this loop to get all header names and their values in all floors
             value = sheet.cell(row, col).value
             door_data[header_name] = value
 
-        floor_doors[room_number] = door_data # 
+        floor_doors[room_number] = door_data
 
     return floor_doors
 
-def collect_all_floors(floor_files):                 # this function to collect all the doors in all floors
+
+# this function to collect all the doors in all floors
+def collect_all_floors(floor_files):
     all_doors = {}
 
     for floor_file in floor_files:                  # this loop to get all the doors in all floors
@@ -44,46 +48,48 @@ def collect_all_floors(floor_files):                 # this function to collect 
         all_doors.update(floor_data)
     return all_doors
 
-def update_main_file(main_file , all_doors):        # this function to update the main file
-    
+
+# this function to update the main file
+def update_main_file(main_file, all_doors):
+
     workbook = openpyxl.load_workbook(main_file)
     sheet = workbook.active
-    
+
     def get_header(sheet):
 
         header = {}             # this dict to store header names and their values
-        
-        for col in range(1, sheet.max_column + 1):    #this loop start from first column to the last column in the row
+
+        # this loop start from first column to the last column in the row
+        for col in range(1, sheet.max_column + 1):
             header_name = sheet.cell(HEADER_ROW, col).value
-            
+
             if header_name is None:
                 continue
             header[header_name] = col
-        
-        return header 
 
+        return header
 
-    def update_doors(sheet , header , all_doors):
-        
+    def update_doors(sheet, header, all_doors):
+
         update_counter = 0
 
-        for row in range(DATA_START_ROW, sheet.max_row + 1):       
+        for row in range(DATA_START_ROW, sheet.max_row + 1):
             room_number = sheet.cell(row, header["ROOM NUMBER"]).value
 
             if room_number is None:
                 continue
 
-            if not room_number :
+            if not room_number:
                 continue
 
             if room_number not in all_doors:
                 continue
-        
-            door_data = all_doors[room_number]          # this dict to get all the doors in all floors
 
-        
+            # this dict to get all the doors in all floors
+            door_data = all_doors[room_number]
+
             for header_name, value in door_data.items():
-                
+
                 if header_name not in header:
                     continue
                 cell_col = header[header_name]
@@ -93,28 +99,29 @@ def update_main_file(main_file , all_doors):        # this function to update th
             update_counter += 1
         return update_counter
 
-    
     header = get_header(sheet)
     update_counter = update_doors(sheet, header, all_doors)
 
-    updated_file = main_file.with_name(main_file.stem + "_updated" + main_file.suffix )  #this line to update the file name
+    # this line to update the file name
+    updated_file = main_file.with_name(
+        main_file.stem + "_updated" + main_file.suffix)
     workbook.save(updated_file)
-    
+
     return update_counter
 
-           
 
-#main
-files = list(Path(r"D:\works\yousef al sdeeq\REV's\14-5-2025").glob("*.xlsx"))    #type hint 
+# main
+files = list(Path(r"secondary file path").glob("*.xlsx"))  # type hint
 
 floor_files = [
     file for file in files if not file.name.startswith("~$")
 ]
-print("reading:", floor_files)    #this line to print the file name
+print("reading:", floor_files)  # this line to print the file name
 
 all_doors = collect_all_floors(floor_files)
 
-main_file = Path(r"D:\works\yousef al sdeeq\Door Schedule\Door Schedule Finil  (yousef al sedek ) @ mahmed ahmed.xlsx")
+# this line to get the main file name
+main_file = Path(r"main file path.xlsx")
 
 
 updated_count = update_main_file(main_file, all_doors)
---
+++ tast.py
@@ -0,0 +1,33 @@
+
+'''for file in files:
+    if file.name.startswith("~$"):
+        continue
+    print(file.name)
+
+
+# should to know sheet.cell(row, col).value = gave me a value in row and col
+
+def get_data(files_path): #first desigen fo rthe script
+    for file in files_path:
+        
+        all_doors = {}
+
+        for row in door_row:
+
+            room_number = "anything"
+            
+            for room_number in room_numbers:
+                
+                
+                if door_number is None:
+                    continue
+
+                door_data = {}
+                
+                for property_column in properties_columns:
+
+                    property_name = "name of property"
+                    property_value = "value of property in call `   
+
+                    door_data[property_name] = property_value
+                all_doors[room_number] = door_data '''
